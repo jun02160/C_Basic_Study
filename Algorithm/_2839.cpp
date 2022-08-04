91adb4b983fb52673a363bd443e338a3b7a0f8c5
jun <jun020216@naver.com>
2022-08-04 19:15:04 +0900

    [boj] 2839

--- Algorithm/_2839.cpp
+++ Algorithm/_2839.cpp
@@ -7,21 +7,19 @@ int main()
     cin >> N;
 
     int cnt = 0;
-    while (N > 0)
+    while (N >= 0)
     {
         if (N % 5 == 0)
         {
             cnt += N / 5;
+            cout << cnt << endl;
+            break;
         }
         N -= 3;
         cnt++;
     }
-
-    if (N == 0)
-    {
-        cout << cnt << endl;
-    }
-    else if (N < 0)
+    
+    if (N < 0)
     {
         cout << "-1" << endl;
     }
